81b95d951efbf02d0af08f2036b0f7856407c678
christian-bromann <mail@christian-bromann.com>
2020-05-24 14:23:36 +0200

    fix: start sauce connect via saucelabs package

--- src/local-tunnel/sauceconnect.ts
+++ src/local-tunnel/sauceconnect.ts
@@ -1,7 +1,4 @@
-import {promisify} from 'util';
-
-// This import lacks type definitions.
-const launchSauceConnect = promisify(require('sauce-connect-launcher-update'));
+import SaucelabsAPI, {SauceConnectInstance} from 'saucelabs';
 
 /**
  * Service that can be used to create a SauceConnect tunnel automatically. This can be used
@@ -10,14 +7,11 @@ const launchSauceConnect = promisify(require('sauce-connect-launcher-update'));
 export function SauceConnect(emitter, logger) {
   const log = logger.create('launcher.SauceConnect');
 
-  // Currently active tunnel instance. See: https://github.com/bermi/sauce-connect-launcher
+  // Currently active tunnel instance. See: https://github.com/saucelabs/node-saucelabs
   // for public API.
   let activeInstancePromise: Promise<any> = null;
 
-  this.establishTunnel = async (connectOptions: any) => {
-    // Redirect all logging output to Karma's logger.
-    connectOptions.logger = log.debug.bind(log);
-
+  this.establishTunnel = async (seleniumCapabilities, sauceConnectOptions: any) => {
     // In case there is already a promise for a SauceConnect tunnel, we still need to return the
     // promise because we want to make sure that the launcher can wait in case the tunnel is
     // still starting.
@@ -26,21 +20,29 @@ export function SauceConnect(emitter, logger) {
     }
 
     // Open a new SauceConnect tunnel.
-    return activeInstancePromise = launchSauceConnect(connectOptions);
+    const api = new SaucelabsAPI(seleniumCapabilities)
+    return activeInstancePromise = api.startSauceConnect({
+        // Redirect all logging output to Karma's logger.
+        logger: log.debug.bind(log),
+        ...sauceConnectOptions
+    });
   };
 
   // Close the tunnel whenever Karma emits the "exit" event. In that case, we don't need to
   // reset the state because Karma will exit completely.
-  emitter.on('exit', (doneFn: () => void) => {
+  emitter.on('exit', async (doneFn: () => void) => {
     if (activeInstancePromise) {
       log.debug('Shutting down Sauce Connect');
 
-      // Close the tunnel and notify Karma once the tunnel has been exited.
-      activeInstancePromise
-        .then(instance => instance.close(doneFn))
-        .catch(() => doneFn())
-    } else {
-      doneFn();
+      // shut down Sauce Connect once all session have been terminated
+      try {
+        const tunnelInstance:SauceConnectInstance = await activeInstancePromise
+        await tunnelInstance.close()
+      } catch (err) {
+        log.error(`Could not close Sauce Connect Tunnel. Failure message: ${err.stack}`);
+      }
     }
+    
+    doneFn();
   })
 }
